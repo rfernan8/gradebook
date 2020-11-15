4bcdceb6e12a88e088509ff4cc46746241d23509
marlee3jackson <34731549+marlee3jackson@users.noreply.github.com>
2020-11-15 01:21:12 -0600

    Update app.py
    all classes are done, please give a once over

--- app.py
+++ app.py
@@ -19,8 +19,6 @@ class student(db.Model):
     first_name = db.Column(db.String(), nullable=False) # define description
     last_name = db.Column(db.String(), nullable=False, default=False)
     
-
-
 class teacher(db.Model):
     __tablename__ = 'teacher'
     tid = db.Column(db.Integer, primary_key=True)
@@ -29,7 +27,36 @@ class teacher(db.Model):
 class parent(db.Model):
     __tablename__ = 'parent'
     pid = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(), nullable=False)    
+    email = db.Column(db.String(), nullable=False) 
+    
+class teaches(db.Model):
+    __tablename__ = 'teaches'
+    tid = db.Column(db.Integer, primary_key=True)
+    cid = db.Column(db.Integer, primary_key=True)    
+    
+ class enrolled(db.Model):
+    __tablename__ = 'enrolled'
+    sid = db.Column(db.Integer, primary_key=True)
+    cid = db.Column(db.Integer, primary_key=True)    
+    
+ class assignment(db.Model):
+    __tablename__ = 'enrolled'
+    cid = db.Column(db.Integer, primary_key=True)
+    aid = db.Column(db.Integer, primary_key=True)    
+    letter = db.Column(db.String(), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    
+ class class(db.Model):
+    __tablename__ = 'class'
+    cid = db.Column(db.Integer, primary_key=True)
+    tid = db.Column(db.Integer, primary_key=True)    
+    name = db.Column(db.String(), nullable=False)
+ 
+class category(db.Model):
+    __tablename__ = 'category'
+    cid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), primary_key=True)    
+    weight = db.Column(db.Weight, nullable=False)
     
     
 # TODO: Edit these functions to match the intended functionality of our database
